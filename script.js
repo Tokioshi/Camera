89535798087b358234fc27780ee28aa950aad097
Tokioshy <grizzlizyping@gmail.com>
2024-06-02 18:14:38 +0700

    Finished

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
+  function setActiveNavLink() {
+    var hash = window.location.hash;
+    $('.nav-link').removeClass('active');
+    if (hash && hash !== '#') {
+      $('.nav-link[href="' + hash + '"]').addClass('active');
+    } else {
+      $('.nav-link[href="#"]').addClass('active');
+    }
+  }
+
   $('.nav-link').on('click', function () {
     $('.nav-link').removeClass('active');
     $(this).addClass('active');
   });
-});
 
-$('#nav-home').addClass('active');
+  $(window).on('hashchange', function () {
+    setActiveNavLink();
+  });
+
+  setActiveNavLink();
+});
